c92e0133ade469e374f67c7b252bacebba967808
Imran Khan <imran.lko@outlook.com>
2016-04-02 12:59:02 +0530

    Basic Solution Added

---
+++ MVAWebApiDesignCourse/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using MVAWebApiDesignCourse.App_Start;
+using System;
+using System.Web.Http;
+
+namespace MVAWebApiDesignCourse
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }    
+    }
+}
